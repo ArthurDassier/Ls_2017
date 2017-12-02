579d8effcb1380fefd81ab458b12c7bfd71ea2f5
Arthur Dassier <arthur.dassier@epitech.eu>
2017-12-02 13:59:48 +0100

    flag l parfaitement fonctionnel

--- srcs/hypl.c
+++ srcs/hypl.c
@@ -12,18 +12,18 @@ void pr_right(char *str, struct stat sb, struct timespec st_ctim,
 {
 	uid_t	uid = getuid();
 	struct passwd	*j = getpwuid(uid);
-	int	size;
-	char	*date;
+	int	size = sb.st_size;
+	char	*date = ctime(&sb.st_ctime);
 	struct group	*gname = getpwuid(uid);
 	stat(str, &sb);
-	size = sb.st_size;
+
 	print_hyp_l(sb);
 	my_printf("%d ", sb.st_nlink);
 	my_printf("%s ", gname->gr_name);
 	my_printf("%s ", j->pw_name);
 	my_printf("%d ", size);
-	date = ctime(&sb.st_ctime);
-	date[my_strlen(date) - 9] = '\0';
-	my_printf("%s ", date);
+	for(int i = 4; i != 16; i++)
+		my_putchar(date[i]);
+	my_putchar(' ');
 	print_my(&str, str, 1);
 }
